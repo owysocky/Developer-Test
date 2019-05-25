563368a3c55f706ad2c9dd265505e18788c493c8
ow <volovykolya@gmail.com>
2019-05-25 11:00:33 -0700

    fix time bug to exevute every time the time%5 has no remainder

--- JuniorDevTestFunctionApp/Functions/ScheduledFunctions.cs
+++ JuniorDevTestFunctionApp/Functions/ScheduledFunctions.cs
@@ -9,41 +9,41 @@
 // -----------------------------------------------------------------------
 namespace JuniorDevTestFunctionApp
 {
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using Microsoft.Azure.WebJobs;
-    using Microsoft.Extensions.Logging;
-    using Newtonsoft.Json.Linq;
-    using Services;
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using Microsoft.Azure.WebJobs;
+  using Microsoft.Extensions.Logging;
+  using Newtonsoft.Json.Linq;
+  using Services;
+
+  /// <summary>
+  /// Class ScheduledFunctions.
+  /// </summary>
+  public static class ScheduledFunctions
+  {
+    /// <summary>
+    /// The cities for weather checks.
+    /// </summary>
+    public static List<string> Cities = new List<string> { "seattle" };
 
     /// <summary>
-    /// Class ScheduledFunctions.
+    /// Runs the scheduled task.
+    /// TODO: This scheduled task has two issues. Please fix these issues:
+    /// TODO: 1) Once we retrieve the data, we need to store the data. Another dev has created a method to do this. Please find it in the WeatherRepository and store this data.
+    /// TODO: 2) The scheduler is running too fast. We need to run it once every 5 minutes.
     /// </summary>
-    public static class ScheduledFunctions
+    /// <param name="timer">The timer.</param>
+    /// <param name="log">The log.</param>
+    /// <returns>Task.</returns>
+    [FunctionName(nameof(RunScheduledTask))]
+    public static async Task RunScheduledTask(
+        [TimerTrigger("0 */5 * * * *")] TimerInfo timer,
+        ILogger log)
     {
-        /// <summary>
-        /// The cities for weather checks.
-        /// </summary>
-        public static List<string> Cities = new List<string> {"seattle"};
-
-        /// <summary>
-        /// Runs the scheduled task.
-        /// TODO: This scheduled task has two issues. Please fix these issues:
-        /// TODO: 1) Once we retrieve the data, we need to store the data. Another dev has created a method to do this. Please find it in the WeatherRepository and store this data.
-        /// TODO: 2) The scheduler is running too fast. We need to run it once every 5 minutes.
-        /// </summary>
-        /// <param name="timer">The timer.</param>
-        /// <param name="log">The log.</param>
-        /// <returns>Task.</returns>
-        [FunctionName(nameof(RunScheduledTask))]
-        public static async Task RunScheduledTask(
-            [TimerTrigger("0 */1 * * * *")] TimerInfo timer,
-            ILogger log)
-        {
-            foreach (var city in Cities)
-            {
-                JObject weather = await WeatherApiService.GetWeather(city);
-            }
-        }
+      foreach (var city in Cities)
+      {
+        JObject weather = await WeatherApiService.GetWeather(city);
+      }
     }
+  }
 }
